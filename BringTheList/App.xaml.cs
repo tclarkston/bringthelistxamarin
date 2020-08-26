bd2ef30caef28df5e57d4a7894b3baf5211c98e6
Todd Clarkston <todd@seriescode.com>
2020-08-25 21:30:00 -0600

    Adding initial checkin for xamarin

---
+++ BringTheList/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using BringTheList.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BringTheList
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ BringTheList/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BringTheList.Models
+{
+    public class Note
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public Note()
+        {
+        }
+    }
+}
