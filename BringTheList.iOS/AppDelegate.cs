fa6460bcbae1b5f9b9bc7fc336da36fad29995c1
todd <todd@wireservers.com>
2021-01-13 22:01:41 -0700

    Updated Ios project with fix for new App()

--- BringTheList.iOS/AppDelegate.cs
+++ BringTheList.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace BringTheList.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new BringTheList.App());
 
             return base.FinishedLaunching(app, options);
         }
